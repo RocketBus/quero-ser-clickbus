73779fc8cca80daf9026374523f82a2cabd4bd92
Fernando <fernando.campanate@hotmail.com>
2019-11-28 22:50:26 -0200

    Added @Repository annotation to PlaceRepository for Visibility

--- testes/backend-developer/solution/clickbus-places/src/main/java/br/com/clickbus/repository/PlaceRepository.java
+++ testes/backend-developer/solution/clickbus-places/src/main/java/br/com/clickbus/repository/PlaceRepository.java
@@ -1,9 +1,11 @@
 package br.com.clickbus.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.clickbus.entity.Place;
 
+@Repository
 public interface PlaceRepository extends CrudRepository<Place, Integer> {
 
 }
