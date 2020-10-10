542510c63a2a93090e5241d8e438026b35e5d621
Rafaela Narcizo <narcizorafaela@gmail.com>
2020-10-10 20:34:58 -0300

    New Empty Activity

---
+++ testes/android-developer/Movie DB Test/app/src/main/java/com/clickbus/moviesdbtest/movies/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.clickbus.moviesdbtest.movies.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.clickbus.moviesdbtest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
