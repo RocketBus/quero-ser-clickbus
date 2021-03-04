8db41d930eda4cd0fb107d04fec3dc2cd6381671
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-03-04 12:53:32 -0300

    create input data

--- testes/cash-machine/src/Cash.java
+++ testes/cash-machine/src/Cash.java
@@ -5,6 +5,9 @@ public class Cash {
 	public static void main(String[] args) {
 		Scanner toRead = new Scanner(System.in);
 		int withdraw;
+		
+		System.out.println("Digite o valor a ser sacado");
+		withdraw = toRead.nextInt();
 	}
 }
 	
